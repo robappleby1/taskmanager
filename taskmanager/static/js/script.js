987b8d73c38801ab3e29f72ae9824a5a04699893
robappleby1 <robappleby1@sky.com>
2022-06-21 05:49:50 +0000

    Start of js code

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // side nav initialized
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.sidenav = M.Sidenav.init(elems, options);
+  });
