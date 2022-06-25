1615e9e0c2d2aa3033b055e302840fdeb4fe586c
robappleby1 <robappleby1@sky.com>
2022-06-25 16:33:48 +0000

    end of video 9

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -5,7 +5,8 @@ from taskmanager.models import Category, Task
 
 @app.route("/")
 def home():
-    return render_template("tasks.html")
+    tasks = list(Task.query.order_by(Task.id).all())
+    return render_template("tasks.html", tasks = tasks)
 
 
 @app.route("/categories")
@@ -48,14 +49,12 @@ def add_task():
     if request.method == "POST":
         task = Task(
             task_name=request.form.get("task_name"),
-            task_description=request.form.get("task_description"),,
+            task_description=request.form.get("task_description"),
             is_urgent=bool(True if request.form.get("is_urgent") else False),
             due_date=request.form.get("due_date"),
-            category_id=request.form.get("category_id"),
+            category_id=request.form.get("category_id")
         )
         db.session.add(task)
         db.session.commit()
         return redirect(url_for("home"))
-    return render_template("add_task.html", categories = categories)
-
-
+    return render_template("add_task.html", categories=categories)
